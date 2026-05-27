78293fa45dd0fd1599794be2a1b72e6c394c642a
igroman4ik-pc <ni4000009@gmail.com>
2026-05-27 11:02:13 +0300

    feat: add auto flow for summary list

--- src/shared/ui/summary-list/summary-list.tsx
+++ src/shared/ui/summary-list/summary-list.tsx
@@ -1,17 +1,11 @@
-import type { ReactNode } from 'react';
-
-import { SimpleGrid } from '@mantine/core';
+import { SimpleGrid, type SimpleGridProps } from '@mantine/core';
 
 import { CONTENT_GRID_SPACING } from '@/shared/model/layout';
 
-export type SummaryListProps = {
-	children: ReactNode;
-};
+export type SummaryListProps = {} & SimpleGridProps;
 
-export function SummaryList({ children }: SummaryListProps) {
+export function SummaryList(props: SummaryListProps) {
 	return (
-		<SimpleGrid minColWidth={300} spacing={CONTENT_GRID_SPACING}>
-			{children}
-		</SimpleGrid>
+		<SimpleGrid minColWidth={300} spacing={CONTENT_GRID_SPACING} autoFlow='auto-fit' {...props} />
 	);
 }
